98e7af3c2bff1cd41bd5ca5266cfd8f111b4e426
Liubomyr <liubomyrromankiv@gmail.com>
2017-08-11 18:09:26 +0300

    Separated functionality

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 
 import user from './user';
 import './main.scss';
+
+let wraper = document.getElementById('wrapper');
+let authorizationForm = document.getElementById('authorization-form');
+
+authorizationForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  let user = {
+  	login: this.login.value,
+  	pass: this.password.value
+  }
+  user.userAuthorization(user);
+  this.login.value = '';
+  this.password.value = '';
+});
--- src/user/index.js
+++ src/user/index.js
@@ -1,20 +1,7 @@
 'use strict';
 
 import users from './users';
-
-let wraper = document.getElementById('wrapper');
-let authorizationForm = document.getElementById('authorization-form');
-
-authorizationForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  let user = {
-  	login: this.login.value,
-  	pass: this.password.value
-  }
-  userAuthorization(user);
-  this.login.value = '';
-  this.password.value = '';
-});
+import dom from '../dom';
 
 let userAuthorization = (user) => {
   let userName = findUser(user);
@@ -38,22 +25,15 @@ let findUser = (user) => {
 let showMessage = (name) => {
   let newDomElement = {};
   if(!!name) {
-    newDomElement = createElement('p',[{'class':'output good'}], 'Hello, ' + name + '. How are you?');
+    newDomElement = dom.createElement('p',[{'class':'output good'}], 'Hello, ' + name + '. How are you?');
   } else {
-    newDomElement = createElement('p',[{'class':'output bad'}], 'No user finded. Please, enter correct "login" and "password"');
+    newDomElement = dom.createElement('p',[{'class':'output bad'}], 'No user finded. Please, enter correct "login" and "password"');
   }
   wraper.appendChild(newDomElement);
 }
 
-let createElement = (element, attriburtesArray, text) => {
-  let newElement = document.createElement(element);
-  if(!!attriburtesArray){
-    for(let i = 0; i < attriburtesArray.length; i++){
-      newElement.setAttribute(attriburtesArray[i].name, attriburtesArray[i].val);
-    }
-  }
-  if(!!text){
-    newElement.innerHTML = text;
-  }
-  return newElement;
+
+
+export default {
+  userAuthorization: userAuthorization
 }
